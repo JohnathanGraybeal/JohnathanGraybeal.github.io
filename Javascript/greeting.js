26cfde981773fc91d5589f8a23547dde9c8c7110
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-05-11 20:38:54 -0400

    javascript to create changing text

---
+++ Javascript/greeting.js
@@ -0,0 +1,14 @@
+
+
+let options = {
+    strings: ['Front-End Developer', 'Back-End Developer', 'Problem Solver', 'Tech Enthusiast', 'Human'],
+    typeSpeed: 30,
+    backDelay: 900,
+    backSpeed: 30,
+    loop: true,
+    showCursor: false
+
+
+};
+
+let typed = new Typed('.greeting-items h1', options);
